d524c78712d7cf8493c7f6734bf9474fdf9fc5f9
efremOffice <143795390+efremOffice@users.noreply.github.com>
2025-03-27 23:12:37 +0300

    test

--- dealers/urls.py
+++ dealers/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-# from .views import add_dealer, get_dealers, update_dealer, delete_all_dealers
+from .views import add_dealer, get_dealers, update_dealer, delete_all_dealers
 
 urlpatterns = [
-    # path('add/', add_dealer, name='add_dealer'),
-    # path('list/', get_dealers, name='get_dealers'),
-    # path('update/<int:dealer_id>/', update_dealer, name='update_dealer'),
-    # path('delete/', delete_all_dealers, name='delete_all_dealers'),
+    path('add/', add_dealer, name='add_dealer'),
+    path('list/', get_dealers, name='get_dealers'),
+    path('update/<int:dealer_id>/', update_dealer, name='update_dealer'),
+    path('delete/', delete_all_dealers, name='delete_all_dealers'),
 ]
