73ce63c2ba614b78983ab6f9ef5679b033c650a2
efremOffice <143795390+efremOffice@users.noreply.github.com>
2025-03-27 11:11:33 +0300

    test

--- dealers/urls.py
+++ dealers/urls.py
@@ -1,9 +1,9 @@
-# dealers/urls.py
 from django.urls import path
-from .views import add_dealer, get_dealers,delete_all_dealers
+from .views import add_dealer, get_dealers, update_dealer, delete_all_dealers
 
 urlpatterns = [
-    path('dealers/', get_dealers, name='get_dealers'),
-    path('dealers/add/', add_dealer, name='add_dealer'),
-    path('delete_all/', delete_all_dealers, name='delete_all'),
+    path('add/', add_dealer, name='add_dealer'),
+    path('list/', get_dealers, name='get_dealers'),
+    path('update/<int:dealer_id>/', update_dealer, name='update_dealer'),
+    path('delete/', delete_all_dealers, name='delete_all_dealers'),
 ]
--- dealers/views.py
+++ dealers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -20,6 +20,16 @@ def get_dealers(request):
     dealers = Dealer.objects.all()
     serializer = DealerSerializer(dealers, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['PUT'])
+def update_dealer(request, dealer_id):
+    """Update an existing dealer."""
+    dealer = get_object_or_404(Dealer, DealerId=dealer_id)
+    serializer = DealerSerializer(dealer, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 def delete_all_dealers(request):
